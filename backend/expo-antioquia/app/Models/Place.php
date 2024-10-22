1fc710174094e9c827297cfd7a1e41f8672d8d05
Maria Martinez <mmariayennifer.25@gmail.com>
2024-10-22 13:56:15 -0500

    Subiendo el backend del proyecto expo-antioquia

---
+++ backend/expo-antioquia/app/Models/Place.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Place extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'location',
+        'climate',
+        'image_url',
+        'video_url',
+        'google_maps',
+        'category',
+        'latitude',
+        'longitude',
+        'services', // Incluye 'services' si es necesario
+        'opening_time',
+        'closing_time',
+        'price_range',
+        'contact_number',
+        'email',
+    ];
+}
