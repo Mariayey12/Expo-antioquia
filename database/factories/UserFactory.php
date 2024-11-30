87dda0844a052ff0bd5b6c1ad4c8671ea5ac545c
Maria Martinez <mmariayennifer.25@gmail.com>
2024-11-30 07:38:03 -0500

    Removed duplicate or unnecessary migration for ads

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,15 +20,16 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => Hash::make('password'), // Contraseña predeterminada encriptada
-            'phone' => $this->faker->optional()->phoneNumber(), // Teléfono opcional
-            'address' => $this->faker->optional()->address(), // Dirección opcional
-            'profile_picture' => $this->faker->optional()->imageUrl(100, 100, 'people', true, 'Perfil'), // Imagen opcional
-            'role' => $this->faker->randomElement(['administrador', 'usuario', 'proveedor']), // Roles predefinidos
-            'userable_type' => null, // Inicialmente null, se define al asignar el modelo polimórfico
-            'userable_id' => null, // Inicialmente null, se define al asignar el modelo polimórfico
+            'password' => Hash::make('password'),
+            'phone' => $this->faker->optional()->phoneNumber(),
+            'address' => $this->faker->optional()->address(),
+            'profile_picture' => $this->faker->optional()->imageUrl(100, 100, 'people', true, 'Perfil'),
+            'role' => $this->faker->randomElement(['administrador', 'usuario', 'proveedor']),
+            'userable_type' => \App\Models\Admin::class, // Modelo relacionado
+            'userable_id' => 1, // ID del modelo relacionado
             'remember_token' => Str::random(10),
         ];
+
     }
 
     /**
