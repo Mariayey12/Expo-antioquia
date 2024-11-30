86f4fa969dd01fcc56e7b6f8a21b7183724cd011
Maria Martinez <mmariayennifer.25@gmail.com>
2024-11-30 15:31:54 -0500

    Removed duplicate or unnecessary migration for ads

--- database/migrations/2024_11_30_150619_create_providers_table.php
+++ database/migrations/2024_11_30_150619_create_providers_table.php
@@ -16,6 +16,7 @@ class CreateProvidersTable extends Migration
             $table->string('email')->nullable();  // Correo electrónico
             $table->string('phone')->nullable();  // Teléfono
             $table->text('address')->nullable();  // Dirección
+            $table->string('password')->nullable();
             $table->text('services')->nullable();
 
             $table->timestamps();
