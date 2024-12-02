0d7d86103c33bcce9bff106ad28561aa00b605f0
Maria Martinez <mmariayennifer.25@gmail.com>
2024-12-02 08:45:20 -0500

    Removed duplicate or unnecessary migration for ads

---
+++ app/Models/Category.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Category extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name'];
+
+    public function places()
+    {
+        return $this->morphedByMany(Place::class, 'categoriables');
+    }
+
+    public function services()
+    {
+        return $this->morphedByMany(Service::class, 'categoriables');
+    }
+
+    public function commerce()
+    {
+        return $this->morphedByMany(Commerce::class, 'categoriables');
+    }
+}
