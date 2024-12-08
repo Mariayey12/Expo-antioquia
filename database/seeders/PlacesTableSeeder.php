3d8144ca2aaaefe3a5d41ce12a253cb70c2d7d08
Maria Martinez <mmariayennifer.25@gmail.com>
2024-12-08 16:34:49 -0500

    actualizacion en backend

--- database/seeders/PlacesTableSeeder.php
+++ database/seeders/PlacesTableSeeder.php
@@ -277,9 +277,9 @@ $serviceData = Service::factory(10)->create();
                 throw new \Exception("El campo '{$key}' no puede ser nulo en el comercio '{$commerceData['name']}'");
             }
         }
-
+        Commerce::create($commerceData);
     }
-    Commerce::create($commerceData);
+
         }
     }
 
