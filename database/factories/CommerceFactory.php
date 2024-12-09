47dc0e33620900d22e82f423023d6313243d675a
Maria Martinez <mmariayennifer.25@gmail.com>
2024-12-08 19:08:34 -0500

    actualizacion en backend

--- database/factories/CommerceFactory.php
+++ database/factories/CommerceFactory.php
@@ -17,11 +17,10 @@ class CommerceFactory extends Factory
             'image_url' => $this->faker->imageUrl(640, 480, 'commerce'),
             'video_url' => $this->faker->url,
             'google_maps' => $this->faker->url,
-            'category' => $this->faker->randomElement(['Retail', 'Restaurant', 'Service', 'Entertainment']),
             'contact_number' => $this->faker->phoneNumber,
             'email' => $this->faker->unique()->safeEmail,
             'website' => $this->faker->url,
-            'categories' => $this->faker->randomElements(['Electronics', 'Fashion', 'Groceries', 'Books'], 2), // Ejemplo de categorías
+
         ];
     }
 }
