7a3e2a088ff9168c39aa872e01dc4a30227f912a
Maria Martinez <mmariayennifer.25@gmail.com>
2024-12-10 22:00:29 -0500

    Descripción de los cambios realizados

--- database/migrations/2024_11_29_222925_users_table.php
+++ database/migrations/2024_11_29_222925_users_table.php
@@ -4,35 +4,38 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+return new class extends Migration
 {
     /**
-     * Ejecutar la migración.
+     * Ejecuta la migración para crear la tabla 'users'.
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')->unique(); // Correo electrónico único
+            $table->timestamp('email_verified_at')->nullable(); // Fecha de verificación del correo
+            $table->string('company_name')->nullable(); // Nombre de la empresa (opcional)
             $table->string('password');
-            $table->string('phone')->nullable();
-            $table->string('address')->nullable();
-            $table->string('profile_picture')->nullable();
-            $table->string('role'); // Admin, Proveedor, Cliente, etc.
+            $table->string('contact_person')->nullable(); // Persona de contacto (opcional)
+            $table->string('phone')->nullable(); // Teléfono (opcional)
+            $table->string('address')->nullable(); // Dirección (opcional)
+            $table->string('profile_picture')->nullable(); // Foto de perfil (opcional)
+            $table->enum('role', ['administrador', 'usuario', 'proveedor'])->default('usuario'); // Roles predefinidos
+            $table->rememberToken(); // Token para recordar sesión
             $table->string('userable_type'); // Tipo del modelo relacionado polimórfico
             $table->unsignedBigInteger('userable_id'); // ID del modelo relacionado polimórfico
-            $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps(); // Tiempos de creación y actualización
         });
     }
 
     /**
-     * Revertir la migración.
+     * Revierte la migración.
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
-}
+};
+
