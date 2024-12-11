dc6915b18e7da4571bdb05066b1441a2a1b7a76e
Maria Martinez <mmariayennifer.25@gmail.com>
2024-12-10 21:57:51 -0500

    Descripción de los cambios realizados

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+
+    public function users()
+    {
+        return $this->morphToMany(User::class, 'userable');
+    }
 }
--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['fecha_reserva', 'hora_reserva', 'cantidad_personas', 'estado', 'notas'];
+
+    /**
+     * Relación polimórfica con usuarios.
+     */
+    public function users()
+    {
+        return $this->morphToMany(User::class, 'userable');
+    }
 }
