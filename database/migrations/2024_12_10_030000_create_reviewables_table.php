a4753815774206cf688de26d271058ddaac103d1
Maria Martinez <mmariayennifer.25@gmail.com>
2024-12-15 18:35:19 -0500

    Descripción de los cambios realizados

--- database/migrations/2024_12_10_030000_create_reviewables_table.php
+++ database/migrations/2024_12_10_030000_create_reviewables_table.php
@@ -1,31 +1,44 @@
 <?php
 
+
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-
-class CreateReservationsTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    public function up(): void
     {
-        Schema::create('reservations', function (Blueprint $table) {
-            $table->id();
+        Schema::create('reviewables', function (Blueprint $table) {
+            $table->id(); // Identificador único
+            $table->unsignedBigInteger('review_id'); // ID de la calificación/reseña
+            $table->unsignedBigInteger('product_id');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->morphs('reservable'); // Relación polimórfica
+            $table->integer('number_of_people');
+            $table->morphs('reviewable'); // Crea columnas `reviewable_id` y `reviewable_type`
             $table->date('reservation_date');
             $table->time('reservation_time');
-            $table->integer('number_of_people');
             $table->enum('status', ['pending', 'confirmed', 'cancelled']);
             $table->text('notes')->nullable();
             $table->timestamps();
+
+            // Llave foránea para la tabla `reviews_califications`
+            $table->foreign('review_id')
+                ->references('id')
+                ->on('reviews_califications')
+                ->onDelete('cascade');
+                $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+    $table->foreign('product_id')
+    ->references('id')
+    ->on('products')
+    ->onDelete('cascade');
         });
     }
 
-    public function down()
+    public function down(): void
     {
-        Schema::dropIfExists('reservations');
+        Schema::dropIfExists('reviewables');
     }
-}
-
-
+};
